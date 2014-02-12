ffa566bf07c4d6275e40678d5869e4912e357b77
Daniel Conroy <daniel.conroy@my.wheaton.edu>
2014-02-11 22:36:23 -0600

    Display functioning! Resizes, works with negative numbers. Currently set for preset timeline.

--- src/timeline/Event.java
+++ src/timeline/Event.java
@@ -11,58 +11,76 @@ package timeline;
 class Event{
 	private String description, title;
 	private int startDate, endDate;
+        private boolean hasStart, hasEnd = false;
 	private Category category;
 	
         public Event(String title, String des, int startDate, int endDate, Category category){
 		this.title = title;
                 this.description = des;
 		this.startDate = startDate;
+                this.hasStart = true;
 		this.endDate = endDate;
+                this.hasEnd = true;
 		this.category = category;
 	}
 	public Event(String title, int startDate, int endDate, Category category){
 		this.title = title;
 		this.startDate = startDate;
+                this.hasStart = true;
 		this.endDate = endDate;
+                this.hasEnd = true;
 		this.category = category;
 	}
 	public Event(String title, int startDate, Category category){
 		this.title = title;
 		this.startDate = startDate;
+                this.hasStart = true;
 		this.category = category;
 	}
 	
 	//Getter methods
 	public String getDescription(){
-		return description;
+            return description;
 	}
 	public String getTitle(){
-		return title;
+            return title;
 	}
+        public boolean hasStartDate(){
+            return hasStart;
+        }
 	public int getStartDate(){
-		return startDate;
+            if(hasStart)
+                return startDate;
+            else
+                return Integer.MIN_VALUE;
 	}
+        public boolean hasEndDate(){
+            return hasEnd;
+        }
 	public int getEndDate(){
-		return endDate;
+            if(hasEnd)
+                return endDate;
+            else
+                return Integer.MAX_VALUE;
 	}
 	public Category getCategory(){
-		return category;
+            return category;
 	}
 	
 	//Setter methods
 	public void setDescription(String description){
-		this.description = description;
+            this.description = description;
 	}
 	public void setTitle(String title){
-		this.title = title;
+            this.title = title;
 	}
 	public void setStartDate(int startDate){
-		this.startDate = startDate;
+            this.startDate = startDate;
 	}
 	public void setEndDate(int endDate){
-		this.endDate = endDate;
+            this.endDate = endDate;
 	}
 	public void setCategory(Category category){
-		this.category = category;
+            this.category = category;
 	}
 }
